1b4e93d6eeaa0dc66741bf8e498c80569b4ff2b2
Sebastian Frysztak <sebastian@frysztak.dev>
2021-09-10 15:07:10 +0200

    Main: add basic CLI parser

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedLabels #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TypeApplications #-}
@@ -14,18 +15,61 @@ import Data.Text
 import Generator (generate)
 import Generator.Fetch
 import Language.TypeScript.Printer (pprint)
-import OpenAPI
+import OpenAPI (decodeOpenApi)
+import Options.Applicative
+import System.Exit
+
+data CLIArgs = CLIArgs
+  { input :: [String],
+    outputDir :: String
+  }
+
+parseStringList :: Monad m => String -> m [String]
+parseStringList = return . Prelude.words
+
+multiString desc = Prelude.concat <$> some single
+  where
+    single = option (str >>= parseStringList) desc
+
+cliParser :: Parser CLIArgs
+cliParser =
+  CLIArgs
+    <$> multiString
+      ( short 'i'
+          <> long "input"
+          <> metavar "JSON"
+          <> help "OpenAPI schema in .json"
+      )
+    <*> strOption
+      ( long "out"
+          <> short 'o'
+          <> metavar "outdir"
+          <> value "api"
+          <> showDefault
+          <> help "Output directory"
+      )
 
 main :: IO ()
-main = do
+main = runGenerator =<< execParser opts
+  where
+    opts =
+      info
+        (cliParser <**> helper)
+        ( fullDesc
+            <> header "OpenAPI client generator"
+        )
+
+runGenerator :: CLIArgs -> IO ()
+runGenerator CLIArgs {input, outputDir} = do
   contents <- BL.readFile "openapi.json"
+
   case decodeOpenApi contents of
-    Left err -> putStrLn $ "Failed to parse JSON: '" ++ err ++ "'"
+    Left err -> die $ "Failed to parse JSON: '" ++ err ++ "'"
     Right openApi ->
       do
         -- let ast = generate openApi [interfaceGenerator, fetchGenerator]
-        let ast = generate openApi [fetchGenerator]
-        putStrLn $ unpack $ pprint ast
+        let modules = generate openApi [fetchGenerator]
+        putStrLn $ unpack $ pprint (modules !! 0)
 
         return ()
 
