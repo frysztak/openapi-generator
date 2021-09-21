b4bb55c082fd56da20377132dbfd5f3a631381bb
Sebastian Frysztak <sebastian@frysztak.dev>
2021-09-21 23:03:22 +0200

    Fetch: add Bearer token support

--- src/Generator.hs
+++ src/Generator.hs
@@ -3,12 +3,45 @@
 module Generator where
 
 import Control.Lens
+import Control.Monad.Reader
 import Data.List (findIndex)
 import Language.TypeScript (Module (Module, body, fileName))
 import OpenAPI (OpenAPI, SchemaOrReference)
 
 class GenerateAST a b where
   genAST :: a -> b
+
+data Env p = Env
+  { openApi :: OpenAPI,
+    param :: p
+  }
+  deriving (Show)
+
+class GenerateAST' a b where
+  genAST' :: Reader (Env a) b
+
+askParam :: Reader (Env a) a
+askParam = do
+  env <- ask
+  pure $ param env
+
+askOpenApi :: Reader (Env a) OpenAPI
+askOpenApi = do
+  env <- ask
+  pure $ openApi env
+
+askEnv :: Reader (Env a) (OpenAPI, a)
+askEnv = do
+  openApi <- askOpenApi
+  param <- askParam
+  pure (openApi, param)
+
+makeEnv :: OpenAPI -> a -> Env a
+makeEnv openApi a =
+  Env
+    { openApi = openApi,
+      param = a
+    }
 
 type Generator = OpenAPI -> [Module]
 
