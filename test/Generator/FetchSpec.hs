8516d307201408ca99b8788783558c85a649c5a0
Sebastian Frysztak <sebastian@frysztak.dev>
2021-09-23 09:46:29 +0200

    Fetch: fix handling of return types in Responses, add tests for getResponseType

---
+++ test/Generator/FetchSpec.hs
@@ -0,0 +1,175 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+module Generator.FetchSpec where
+
+import Data.Aeson (decode)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map.Strict as M
+import Data.Text
+import Generator.Common
+import Generator.Fetch
+import Language.TypeScript
+import OpenAPI
+import Test.Hspec
+import Text.RawString.QQ (r)
+
+spec :: Spec
+spec = do
+  describe "getResponseType" $ do
+    let decodeResponses = decode :: BS.ByteString -> Maybe Responses
+
+    it "can process refs" $ do
+      let responses =
+            decodeResponses
+              [r|
+{
+  "200": {
+    "description": "successful operation",
+    "content": {
+      "application/json": {
+        "schema": {
+          "$ref": "#/components/schemas/ApiResponse"
+        }
+      }
+    }
+  }
+}
+|]
+      responses `shouldNotBe` Nothing
+      responses `shouldNotBe` Just M.empty
+
+      let responseType = getResponseType =<< responses
+      responseType `shouldBe` Just (QualifiedName "M" $ TypeRef "ApiResponse")
+
+    it "can process empty inline schema" $ do
+      let responses =
+            decodeResponses
+              [r|
+{
+  "200": {
+    "description": "successful operation",
+    "content": {
+      "application/json": {
+        "schema": {
+          "type": "object",
+          "additionalProperties": {
+            "type": "integer",
+            "format": "int32"
+          }
+        }
+      }
+    }
+  }
+}
+|]
+      responses `shouldNotBe` Nothing
+      responses `shouldNotBe` Just M.empty
+
+      let responseType = getResponseType =<< responses
+      responseType `shouldBe` Just (Object M.empty)
+
+    it "can process string schema" $ do
+      let responses =
+            decodeResponses
+              [r|
+{
+  "200": {
+    "description": "successful operation",
+    "content": {
+      "application/json": {
+        "schema": {
+          "type": "string"
+        }
+      }
+    }
+  }
+}
+|]
+      responses `shouldNotBe` Nothing
+      responses `shouldNotBe` Just M.empty
+
+      let responseType = getResponseType =<< responses
+      responseType `shouldBe` Just String
+
+    it "can process string array schema" $ do
+      let responses =
+            decodeResponses
+              [r|
+{
+  "200": {
+    "description": "successful operation",
+    "content": {
+      "application/json": {
+        "schema": {
+          "type": "array",
+          "items": {
+            "type": "string"
+          }
+        }
+      }
+    }
+  }
+}
+|]
+      responses `shouldNotBe` Nothing
+      responses `shouldNotBe` Just M.empty
+
+      let responseType = getResponseType =<< responses
+      responseType `shouldBe` Just (List String)
+
+    it "can process ref array schema" $ do
+      let responses =
+            decodeResponses
+              [r|
+{
+  "200": {
+    "description": "successful operation",
+    "content": {
+      "application/json": {
+        "schema": {
+          "type": "array",
+          "items": {
+            "$ref": "#/components/schemas/Pet"
+          }
+        }
+      }
+    }
+  }
+}
+|]
+      responses `shouldNotBe` Nothing
+      responses `shouldNotBe` Just M.empty
+
+      let responseType = getResponseType =<< responses
+      responseType `shouldBe` Just (List (QualifiedName "M" (TypeRef "Pet")))
+
+    it "can process ref 2D array schema" $ do
+      let responses =
+            decodeResponses
+              [r|
+{
+  "200": {
+    "description": "successful operation",
+    "content": {
+      "application/json": {
+        "schema": {
+          "type": "array",
+          "items": {
+            "type": "array",
+            "items": {
+              "$ref": "#/components/schemas/Pet"
+            }
+          }
+        }
+      }
+    }
+  }
+}
+|]
+      responses `shouldNotBe` Nothing
+      responses `shouldNotBe` Just M.empty
+
+      let responseType = getResponseType =<< responses
+      responseType `shouldBe` Just (List $ List (QualifiedName "M" (TypeRef "Pet")))
